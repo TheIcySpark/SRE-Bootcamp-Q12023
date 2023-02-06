09768abe4db94d686f78e3d440b579214b2da997
Isaac Manjarrez Leyva <54146445+TheIcySpark@users.noreply.github.com>
2023-02-05 19:17:40 -0600

    Challenge completed

--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,11 +1,42 @@
-# These functions need to be implemented
+import mysql.connector
+import hashlib
+import jwt
+from flask import abort
+
+database_connection = mysql.connector.connect(
+    host='sre-bootcamp-selection-challenge.cabf3yhjqvmq.us-east-1.rds.amazonaws.com',
+    user='secret',
+    password='jOdznoyH6swQB9sTGdLUeeSrtejWkcw',
+    database='bootcamp_tht'
+)
+
+encrypt_token = 'my2w7wjd7yXF64FIADfJxNs1oupTGAuW'
+
 class Token:
 
     def generate_token(self, username, password):
-        return 'test'
+        cursor = database_connection.cursor()
+        cursor.execute('SELECT * FROM users where username = %s LIMIT 1', (username,))
+        data = cursor.fetchone()
+
+        if data:
+            hash = hashlib.sha512(
+                str(password + data[2]).encode('utf-8')).hexdigest()
+            if hash == data[1]:
+                return jwt.encode({'role': data[3]}, encrypt_token)
+        abort(403)
 
 
 class Restricted:
 
-    def access_data(self, authorization):
-        return 'test'
+    def access_data(self, authorization: str):
+        decoded_token = jwt.decode(
+            authorization, encrypt_token, algorithms=['HS256'])
+            
+        cursor = database_connection.cursor()
+        cursor.execute('SELECT role FROM users where role = %s LIMIT 1', (decoded_token['role'],))
+        role = cursor.fetchone()
+
+        if role:
+            return 'You are under protected data'
+        abort(403)
--- auth_api/python/tests.py
+++ auth_api/python/tests.py
@@ -1,5 +1,7 @@
 import unittest
 from methods import Token, Restricted
+from werkzeug import exceptions
+
 
 
 class TestStringMethods(unittest.TestCase):
@@ -8,11 +10,21 @@ class TestStringMethods(unittest.TestCase):
         self.convert = Token()
         self.validate = Restricted()
 
+
     def test_generate_token(self):
         self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI', self.convert.generate_token('admin', 'secret'))
 
     def test_access_data(self):
         self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI'))
+
+    def test_access_token_bob(self):
+        self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidmlld2VyIn0.l7pxJXYHlJdtI9RME2UesMzuVjqf-RtzQeLTHomo_Ic', self.convert.generate_token('bob', 'thisIsNotAPasswordBob'))
+    
+    def test_check_role_exists(self):
+        with self.assertRaises(exceptions.Forbidden):
+            self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiTk9ORSJ9.AxA08Nf3JLoxjlXsIVv0BeHE4tVSEs-u60gJDm2Dk4U')
+
+        
 
 if __name__ == '__main__':
     unittest.main()
